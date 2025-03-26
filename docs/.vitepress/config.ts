d8b53b29342e49b35cf5b7d7b6bf8fb911696dd4
will <will1148683448@gmail.com>
2025-03-26 15:34:27 +0800

    创建文档插件vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  // srcDir: 'components', // 配置存放markdown文件并生成对应路由的文件夹
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'General 超大杯组件',
+        items: [
+          {text: 'Button 按钮', link: '/components/utp-button'}
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/components/utp-button.md
@@ -0,0 +1,8 @@
+---
+title: Button（按钮）- Utopia UI
+description: 用于触发即时操作
+---
+
+# Button 按钮
+用于触发即时操作
+## 基本使用
