7073c1f2a851d018f605238fde73bee1e057871e
will <will1148683448@gmail.com>
2025-03-31 22:40:53 +0800

    UtpSelect组件创建

---
+++ src/components/UtpSelect/types.ts
@@ -0,0 +1,18 @@
+interface SelectOptions {
+  lable: string // 选项名称
+  value: string // 选项值
+  disabled: boolean
+}
+interface UtpSelectProps {
+  modelValue: string  // 当前选中的选项
+  options: SelectOptions[]  // 选项列表
+  placeholder: string
+  disabled: boolean
+}
+interface UtpSelectEmits {
+  (e: 'change', value: string) : void
+  (e: 'update:modelValue', value: string) : void
+
+}
+
+export type { UtpSelectProps, UtpSelectEmits, SelectOptions}
