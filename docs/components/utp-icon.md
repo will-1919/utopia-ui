2b4ffd4d9b2ed922ba5e835ff501b52a57f29baa
will <will1148683448@gmail.com>
2025-04-28 22:41:25 +0800

    ICON文档撰写

--- docs/components/utp-icon.md
+++ docs/components/utp-icon.md
@@ -14,3 +14,14 @@ description: 语义化的矢量图形
 <small style="color: var(--utp-color-primary);text-align:right;display:block;">ps：不支持pro角标的图标哦，露出贫穷的微笑
 (￣ω￣)</small>
 <preview path="../demo/UtpIcon/Basic.vue" title="常用图标" description=""></preview>
+
+## Button API
+
+支持fontawesome的所有API，另外多加了自定义的API, 有关fontawesome的API请查看官网[官方文档](https://docs.fontawesome.com/apis)，或[中文文档](https://fontawesome.com.cn/docs/v5/desktop)
+
+#### 属性
+
+| 属性  | 说明         | 类型                                       | 默认值 |
+| ----- | ------------ | ------------------------------------------ | ------ |
+| type  | 设置图标主题 | `primary` \| `success` \| `info`\| `error` | —      |
+| color | 设置图标颜色 | `string`                                   | —      |
--- docs/demo/UtpIcon/Basic.vue
+++ docs/demo/UtpIcon/Basic.vue
@@ -7,10 +7,62 @@ import UtpIcon from '@/components/UtpIcon/UtpIcon.vue'
 <template>
   <!-- 常用图标 -->
   <div class="group-box">
-    <p>Classic</p>
+    <p>Classic Solid</p>
     <div class="group-content">
-      <utp-icon icon="fa-house"></utp-icon>
+      <utp-icon icon="fa-solid fa-house"></utp-icon>
+      <utp-icon icon="fa-solid fa-circle-user"></utp-icon>
+      <utp-icon icon="fa-solid fa-image"></utp-icon>
+      <utp-icon icon="fa-solid fa-file"></utp-icon>
+      <utp-icon icon="fa-solid fa-camera"></utp-icon>
+      <utp-icon icon="fa-solid fa-calendar"></utp-icon>
+      <utp-icon icon="fa-solid fa-cloud"></utp-icon>
+      <utp-icon icon="fa-solid fa-truck"></utp-icon>
+      <utp-icon icon="fa-solid fa-thumbs-up"></utp-icon>
+      <utp-icon icon="fa-solid fa-face-smile"></utp-icon>
+      <utp-icon icon="fa-solid fa-headphones"></utp-icon>
+      <utp-icon icon="fa-solid fa-bell"></utp-icon>
+      <utp-icon icon="fa-solid fa-user"></utp-icon>
+      <utp-icon icon="fa-solid fa-comment"></utp-icon>
+      <utp-icon icon="fa-solid fa-envelope"></utp-icon>
+      <utp-icon icon="fa-solid fa-magnifying-glass"></utp-icon>
+    </div>
+    <p>Classic Regular</p>
+    <div class="group-content">
+      <utp-icon icon="fa-regular fa-circle-user"></utp-icon>
+      <utp-icon icon="fa-regular fa-image"></utp-icon>
+      <utp-icon icon="fa-regular fa-file"></utp-icon>
+      <utp-icon icon="fa-regular fa-calendar"></utp-icon>
+      <utp-icon icon="fa-regular fa-thumbs-up"></utp-icon>
+      <utp-icon icon="fa-regular fa-face-smile"></utp-icon>
+      <utp-icon icon="fa-regular fa-bell"></utp-icon>
+      <utp-icon icon="fa-regular fa-user"></utp-icon>
+      <utp-icon icon="fa-regular fa-comment"></utp-icon>
+      <utp-icon icon="fa-regular fa-envelope"></utp-icon>
+      <utp-icon icon="fa-regular fa-font-awesome"></utp-icon>
+      <utp-icon icon="fa-regular fa-star"></utp-icon>
+      <utp-icon icon="fa-regular fa-heart"></utp-icon>
+      <utp-icon icon="fa-regular fa-circle-xmark"></utp-icon>
+      <utp-icon icon="fa-regular fa-calendar-days"></utp-icon>
+      <utp-icon icon="fa-regular fa-clipboard"></utp-icon>
+    </div>
+    <p>Classic Brands</p>
+    <div class="group-content">
+      <utp-icon icon="fa-brands fa-font-awesome"></utp-icon>
+      <utp-icon icon="fa-brands fa-web-awesome"></utp-icon>
       <utp-icon icon="fa-brands fa-github"></utp-icon>
+      <utp-icon icon="fa-brands fa-bluesky"></utp-icon>
+      <utp-icon icon="fa-brands fa-facebook"></utp-icon>
+      <utp-icon icon="fa-brands fa-linkedin"></utp-icon>
+      <utp-icon icon="fa-brands fa-discord"></utp-icon>
+      <utp-icon icon="fa-brands fa-slack"></utp-icon>
+      <utp-icon icon="fa-brands fa-algolia"></utp-icon>
+      <utp-icon icon="fa-brands fa-figma"></utp-icon>
+      <utp-icon icon="fa-brands fa-youtube"></utp-icon>
+      <utp-icon icon="fa-brands fa-apple"></utp-icon>
+      <utp-icon icon="fa-brands fa-google"></utp-icon>
+      <utp-icon icon="fa-brands fa-pied-piper-hat"></utp-icon>
+      <utp-icon icon="fa-brands fa-kickstarter"></utp-icon>
+      <utp-icon icon="fa-brands fa-docker"></utp-icon>
     </div>
   </div>
 </template>
@@ -23,5 +75,6 @@ import UtpIcon from '@/components/UtpIcon/UtpIcon.vue'
     display: flex;
     justify-content: space-between;
     align-items: center;
+    flex-wrap: wrap;
   }
 </style>
