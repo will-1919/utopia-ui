fff4b5a3450f09ec5aeaaace49e9a04e75f546bd
will <will1148683448@gmail.com>
2025-06-24 22:51:13 +0800

    icon以及样式文件打包配置

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,15 @@ import UtpMessage, { createMessage, closeAll as closeMessageAll } from '@/compon
 import UtpSelect from '@/components/UtpSelect'
 import UtpSwitch from '@/components/UtpSwitch'
 import UtpTooltip from '@/components/UtpTooltip'
+// 处理样式
+import './styles/index.css'
+// 处理图标
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
+library.add(fab, fas, far) // 添加图标到项目
 const components = [
   UtpButton,
   UtpCollapse,
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts' // 引入vite的.d.ts打包插件
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), dts(
+    {tsconfigPath: './tsconfig.build.json'}
+  )],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -16,9 +19,18 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'UtpElement',
-      fileName: 'utp-element'
+      entry: resolve(__dirname, 'src/index.ts'),  // 构建库的入口文件
+      name: 'UtpElement', // 指定库的全局变量名（当构建格式为 UMD 或 IIFE 时会用到），挂载在 window上，console.log(window.UtpElement)
+      fileName: 'utp-element' // 指定输出的文件名
+    },
+    rollupOptions: {
+      external: ['vue', '@fortawesome/fontawesome-svg-core', '@fortawesome/vue-fontawesome', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons'],  // 配置外部依赖，不打包进输出的库中
+      output: {
+        exports: 'named', // 指定导出方式为命名导出（export const MyComponent = { }）
+        globals: {
+          vue: 'Vue',  // 如果使用 UMD/IIFE 构建方式，vue 这个外部模块对应的全局变量是 Vue
+        }
+      }
     }
   }
 })
