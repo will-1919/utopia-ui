773308d397ed5b40351468a2f32dfa143ef62033
will <will1148683448@gmail.com>
2025-06-25 11:39:10 +0800

    拆分umd和es打包配置文件

---
+++ vite.es.config.ts
@@ -0,0 +1,48 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts' // 引入vite的.d.ts打包插件
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+      outDir: 'dist/types',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist/es',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'), // 构建库的入口文件
+      name: 'UtpElement', // 指定库的全局变量名（当构建格式为 UMD 或 IIFE 时会用到），挂载在 window上，console.log(window.UtpElement)
+      fileName: 'utp-element', // 指定输出的文件名
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/vue-fontawesome',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-brands-svg-icons',
+        '@fortawesome/free-regular-svg-icons',
+        '@popperjs/core',
+        'async-validator',
+      ], // 配置外部依赖，不打包进输出的库中
+      output: {},
+    },
+  },
+})
---
+++ vite.umd.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),  // 构建库的入口文件
+      name: 'UtpElement', // 指定库的全局变量名（当构建格式为 UMD 或 IIFE 时会用到），挂载在 window上，console.log(window.UtpElement)
+      fileName: 'utp-element', // 指定输出的文件名
+      formats: ['umd']
+    },
+    outDir: 'dist/umd',
+    rollupOptions: {
+      external: ['vue'],  // 配置外部依赖，不打包进输出的库中
+      output: {
+        exports: 'named', // 指定导出方式为命名导出（export const MyComponent = { }）
+        globals: {
+          vue: 'Vue',  // 如果使用 UMD/IIFE 构建方式，vue 这个外部模块对应的全局变量是 Vue
+        }
+      }
+    }
+  }
+})
